b94525676326cbc2bac96cfeda264ff2e4f64c65
EvgeniiaLiashenko <jennietols@gmail.com>
2022-04-05 11:29:46 +0300

    added adaptiv

--- Portfolio/app/js/main.js
+++ Portfolio/app/js/main.js
@@ -5,19 +5,17 @@ $(document).ready(function() {
     });
 
     $(".menu a").on("click", function(event) {
-        //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
 
-        //забираем идентификатор бока с атрибута href
         var id = $(this).attr('href'),
 
-            //узнаем высоту от начала страницы до блока на который ссылается якорь
             top = $(id).offset().top;
 
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({ scrollTop: top }, 1500);
+        $('body,html').animate({ scrollTop: top }, 1000);
     });
 
-    var mixer = mixitup('.portfolio__content')
+    var mixer = mixitup('.portfolio__content');
+
+
 
 });
